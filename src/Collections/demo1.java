57f3e88542a016710dec3cd8cc3b493aaec0a71b
DaGuoAiHeLiangCha <wmmxsd@126.com>
2019-05-10 14:32:29 +0800

    update

--- src/Collections/demo1.java
+++ src/Collections/demo1.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class demo1 {
     public static void main(String[] args) {
         List list = new ArrayList<>();
+        ((ArrayList) list).trimToSize();
         list.add("321");
         list.add("123");
         list.add(1,123);
