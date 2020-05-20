a471240324e3e380d3ae02a145eed4345aac4a0f
wmmxsd@126.com <w7610232mm>
2020-05-20 16:28:05 +0800

    静态内部类和非静态内部类区别demo

--- src/main/java/com/wmm/basics/keyword/staticdemo/App.java
+++ src/main/java/com/wmm/basics/keyword/staticdemo/App.java
@@ -6,4 +6,8 @@ package com.wmm.basics.keyword.staticdemo;
  * @date @2020/5/20 15:36
  */
 public class App {
+    public static void main(String[] args) {
+        StaticInnerClassDemo.StaticInnerClass staticInnerClass = new StaticInnerClassDemo.StaticInnerClass();
+        StaticInnerClassDemo.InnerClass innerClass = new StaticInnerClassDemo().new InnerClass();
+    }
 }
--- src/main/java/com/wmm/basics/keyword/staticdemo/StaticInnerClassDemo.java
+++ src/main/java/com/wmm/basics/keyword/staticdemo/StaticInnerClassDemo.java
@@ -6,4 +6,44 @@ package com.wmm.basics.keyword.staticdemo;
  * @date @2020/5/20 15:28
  */
 public class StaticInnerClassDemo {
+    private String str1 = "1";
+    private static String str2 = "2";
+
+    public void test() {
+        System.out.println("test");
+    }
+
+    public static void test1() {
+        System.out.println("test1");
+    }
+    public class InnerClass{
+        private String str3 = str1;
+        private String str4 = str2;
+        //Inner classes cannot have static declarations
+        //private static String staticStr = str2;
+
+        public void test2() {
+            test();
+        }
+
+        public void test3() {
+            test1();
+        }
+    }
+
+    public static class StaticInnerClass{
+        //Non-static field 'str1' cannot be referenced from a static context
+        //private String str5 = str1;
+        private String str6 = str2;
+        private static String str7 = str2;
+
+        public void test4() {
+            //Non-static method 'test()' cannot be referenced from a static contex
+            //test();
+        }
+
+        public void test5() {
+            test1();
+        }
+    }
 }
