766eb311d62cce596c2951915e01116d5209781c
wmmxsd@126.com <w7610232mm>
2020-05-20 15:44:35 +0800

    123

---
+++ src/main/java/com/wmm/basics/keyword/staticdemo/App.java
@@ -0,0 +1,9 @@
+package com.wmm.basics.keyword.staticdemo;
+
+/**
+ * @author wangmingming160328
+ * @Description
+ * @date @2020/5/20 15:36
+ */
+public class App {
+}
---
+++ src/main/java/com/wmm/basics/keyword/staticdemo/StaticInnerClassDemo.java
@@ -0,0 +1,9 @@
+package com.wmm.basics.keyword.staticdemo;
+
+/**
+ * @author wangmingming160328
+ * @Description
+ * @date @2020/5/20 15:28
+ */
+public class StaticInnerClassDemo {
+}
