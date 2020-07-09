b3a61497eb997629e68bdc192ac1f5238c2e7c5c
Administrator <wmmxsd@126.com>
2020-07-09 22:44:57 +0800

    upate

---
+++ src/main/java/com/wmm/basics/basicdatatypes/CharDemo.java
@@ -0,0 +1,20 @@
+package com.wmm.basics.basicdatatypes;
+
+/**
+ * char类型demo
+ * @author wangmingming160328
+ * @Description
+ * @date @2020/5/20 22:20
+ */
+public class CharDemo {
+    static int a;
+
+    public static void main(String[] args) {
+        char[] ch = {'a', 'b', 'c'};
+        System.out.println(ch);
+       String a = "abc";
+       System.out.println(a.equals(ch));
+       int c = 1;
+       Object object = c; 
+    }
+}
