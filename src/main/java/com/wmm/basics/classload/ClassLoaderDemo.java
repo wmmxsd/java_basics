87a1b261790b76efd0e372c245167c9ef57e2790
wmmxsd@126.com <w7610232mm>
2020-08-18 09:40:48 +0800

    add systemClassLoader output

--- src/main/java/com/wmm/basics/classload/ClassLoaderDemo.java
+++ src/main/java/com/wmm/basics/classload/ClassLoaderDemo.java
@@ -7,9 +7,10 @@ package com.wmm.basics.classload;
  */
 public class ClassLoaderDemo {
     public static void main(String[] args) {
-        System.out.println("ClassLoaderDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader());
-        System.out.println("The Parent of ClassLoaderDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent());
-        System.out.println("The GrandParent of ClassLoaderDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
+        System.out.println("ClassLoader.getSystemClassLoader():" + ClassLoader.getSystemClassLoader());
+        System.out.println("ClassLoaderDemo.class.getClassLoader():" + ClassLoaderDemo.class.getClassLoader());
+        System.out.println("ClassLoaderDemo.class.getClassLoader().getParent():" + ClassLoaderDemo.class.getClassLoader().getParent());
+        System.out.println("ClassLoaderDemo.class.getClassLoader().getParent().getParent():" + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
         System.out.println("AppClassLoader的父类加载器为ExtClassLoader ExtClassLoader的父类加载器为null，null并不代表ExtClassLoader没有父类加载器，而是 BootstrapClassLoader 。");
     }
 }
