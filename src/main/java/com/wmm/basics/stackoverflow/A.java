b4853deb1ea42ff2fb71a21a243cbcfd82093511
Administrator <wmmxsd@126.com>
2020-08-23 18:59:28 +0800

    stackOverFlow demo

---
+++ src/main/java/com/wmm/basics/stackoverflow/A.java
@@ -0,0 +1,15 @@
+package com.wmm.basics.stackoverflow;
+
+/**
+ * @author wangmingming160328
+ * @Description
+ * @date @2020/8/23 18:52
+ */
+public class A {
+    private B b = new B();
+
+    @Override
+    public String toString() {
+        return "A{" + "b=" + b + '}';
+    }
+}
---
+++ src/main/java/com/wmm/basics/stackoverflow/B.java
@@ -0,0 +1,15 @@
+package com.wmm.basics.stackoverflow;
+
+/**
+ * @author wangmingming160328
+ * @Description
+ * @date @2020/8/23 18:53
+ */
+public class B {
+    private A a = new A();
+
+    @Override
+    public String toString() {
+        return "B{" + "a=" + a + '}';
+    }
+}
