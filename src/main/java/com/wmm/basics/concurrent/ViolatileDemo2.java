696f2b852d3dbda00396cf0e022009db3147d8b2
wmmxsd@126.com <w7610232mm>
2021-04-02 16:26:06 +0800

    update volatile demo

--- src/main/java/com/wmm/basics/concurrent/ViolatileDemo2.java
+++ src/main/java/com/wmm/basics/concurrent/ViolatileDemo2.java
@@ -1,6 +1,7 @@
 package com.wmm.basics.concurrent;
 
 /**
+ * volatile不具备原子性
  * @author wangmingming160328
  * @Description
  * @date @2019/10/21 10:50
@@ -8,24 +9,21 @@ package com.wmm.basics.concurrent;
 public class ViolatileDemo2 {
     public static volatile int t = 0;
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Thread[] threads = new Thread[10];
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             //每个线程对t进行1000次加1的操作
-            threads[i] = new Thread(new Runnable(){
-                @Override
-                public void run(){
-                    for(int j = 0; j < 1000; j++){
-                        t = t + 1;
-                    }
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    t = t + 1;
                 }
             });
             threads[i].start();
         }
 
         //等待所有累加线程都结束
-        while(Thread.activeCount() > 1){
+        while (Thread.activeCount() > 1) {
+            Thread.currentThread().getThreadGroup().list();
             Thread.yield();
         }
 
--- src/main/java/com/wmm/basics/concurrent/VolatileDemo.java
+++ src/main/java/com/wmm/basics/concurrent/VolatileDemo.java
@@ -1,23 +1,22 @@
 package com.wmm.basics.concurrent;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
+ * 增加可重入锁，实现原子性
  * @author wangmingming160328
  * @Description
  * @date @2019/10/18 17:23
  */
 public class VolatileDemo {
-
-    public AtomicInteger inc = new AtomicInteger(0);
-
+    //volatile可加可不加，因为reentrantLock本来就具备原子性
+    public volatile int inc = 0;
     ReentrantLock reentrantLock = new ReentrantLock(false);
 
     public void increase() {
         reentrantLock.lock();
         try {
-            inc.getAndIncrement();
+            inc++;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -27,19 +26,17 @@ public class VolatileDemo {
 
     public static void main(String[] args) {
         final VolatileDemo test = new VolatileDemo();
-        for(int i=0;i<10;i++){
-            new Thread(){
-                @Override
-                public void run() {
-                    for(int j=0;j<1000;j++) {
-                        test.increase();
-                    }
-                };
-            }.start();
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000000; j++) {
+                    test.increase();
+                }
+            }).start();
         }
 
-        while(Thread.activeCount()>1)  //保证前面的线程都执行完
-        {
+        //保证前面的线程都执行完
+        while (Thread.activeCount() > 1) {
+            Thread.currentThread().getThreadGroup().list();
             Thread.yield();
         }
         System.out.println(test.inc);
